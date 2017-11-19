87142d847fc063c6d39885e1f3ec46f5d3a37964
chunxing <chunxing@fit2cloud.com>
2017-11-19 23:01:05 +0800

    index

---
+++ login/src/main/java/com/cloud/login/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.cloud.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
